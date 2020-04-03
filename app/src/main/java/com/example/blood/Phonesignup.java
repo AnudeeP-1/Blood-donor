3a1c72de6d76302cfc510c17733ca0935128c46b
nhmayya <nhmayya@gmail.com>
2020-04-03 20:09:57 +0530

    otp done(error)

--- app/src/main/java/com/example/blood/Phonesignup.java
+++ app/src/main/java/com/example/blood/Phonesignup.java
@@ -9,11 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Phonesignup extends AppCompatActivity {
 
     Button sendotp;
-
+    TextInputLayout regPhoneNo;
+    //Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +26,30 @@ public class Phonesignup extends AppCompatActivity {
         setContentView(R.layout.activity_phonesignup);
 
         sendotp = findViewById(R.id.phonesignupsendotp);
-
+        regPhoneNo = findViewById(R.id.phonesignupnum);
         sendotp.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View view) {
+                String phoneno=regPhoneNo.getEditText().getText().toString();
 
                 Intent intent = new Intent(Phonesignup.this, VerifyPhone.class);
+                intent.putExtra("phone",phoneno);
                 startActivity(intent);
             }
 
 
         });
 
+        //regBtn = findViewById(R.id.phonesignupsendotp);
+
     }
+
+    /*public void registerUser(View view) {
+        String phoneno=regPhoneNo.getText().toString();
+        Intent intent=new Intent(getApplicationContext(),VerifyPhone.class);
+        intent.putExtra("phone",phoneno);
+        startActivity(intent);
+
+    }*/
 }
--- app/src/main/java/com/example/blood/VerifyPhone.java
+++ app/src/main/java/com/example/blood/VerifyPhone.java
@@ -1,5 +1,6 @@
 package com.example.blood;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +13,21 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class VerifyPhone extends AppCompatActivity {
 
@@ -23,7 +36,8 @@ public class VerifyPhone extends AppCompatActivity {
     TextView sloganText;
     TextInputLayout otp;
     Button verify;
-
+    ProgressBar progressBar;
+    String verificationCodeBySystem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +49,10 @@ public class VerifyPhone extends AppCompatActivity {
         sloganText = findViewById(R.id.otpsent);
         otp = findViewById(R.id.otp);
         verify = findViewById(R.id.verify);
-
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
+        String Phoneverify = getIntent().getStringExtra("phone");
+        sendVerificationCodeToUser(Phoneverify);
         verify.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
@@ -52,7 +69,72 @@ public class VerifyPhone extends AppCompatActivity {
                     ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(VerifyPhone.this, pairs);
                     startActivity(intent, options.toBundle());
                 }
+                String code = otp.getEditText().getText().toString();
+
+                if (code.isEmpty() || code.length() < 6) {
+                    otp.setError("Wrong OTP...");
+                    otp.requestFocus();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                verifyCode(code);
+
             }
         });
     }
+    private void verifyCode(String verificationCode) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCodeBySystem, verificationCode);
+        signInTheUserByCredentials(credential);
+    }
+    private void signInTheUserByCredentials(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(VerifyPhone.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(VerifyPhone.this, "Your Account has been created successfully!", Toast.LENGTH_SHORT).show();
+                            //Perform Your required action here to either let the user sign In or do something required
+                            // Intent intent = new Intent(getApplicationContext(), Login.class);
+                            //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            //startActivity(intent);
+
+                        } else {
+                            Toast.makeText(VerifyPhone.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void sendVerificationCodeToUser(String Phoneverify) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + Phoneverify,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                TaskExecutors.MAIN_THREAD,   // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks =
+            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                @Override
+                public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                    super.onCodeSent(s, forceResendingToken);
+                    //Get the code in global variable
+                    verificationCodeBySystem = s;
+                }
+                @Override
+                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                    String code = phoneAuthCredential.getSmsCode();
+                    if (code != null) {
+                        progressBar.setVisibility(View.VISIBLE);
+                        verifyCode(code);
+                    }
+
+                }
+                @Override
+                public void onVerificationFailed(@NonNull FirebaseException e) {
+                    Toast.makeText(VerifyPhone.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+
+            };
 }
